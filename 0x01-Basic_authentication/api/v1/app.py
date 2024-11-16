874e1f63fb877b2765c3e81bd3f3bcd2c3a153d2
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-11-16 03:14:40 +0200

    update: add before_request function with some checks

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth:
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
